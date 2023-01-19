02791803af9da1a8deb228640872ad8f7142c8b7
aslak <aslakrb@gmail.com>
2023-01-19 17:48:25 +0100

    now adding new ads by id when found instead of replacing file

--- parseAds.ts
+++ parseAds.ts
@@ -3,14 +3,40 @@ import {
   removeFile,
   writeJSON,
 } from "https://deno.land/x/flat@0.0.14/mod.ts";
+
+import * as R from "https://deno.land/x/ramda@v0.27.2/mod.ts";
+
 // The filename is the first invocation argument
 const filename = Deno.args[0];
-const data = await readJSON(filename);
+const output = `filtered_${filename}`;
 
-// Pluck a specific key off
-// and write it out to a different file
-// Careful! any uncaught errors and the workflow will fail, committing nothing.
-const newfile = `filtered_${filename}`;
-await writeJSON(newfile, data.docs);
+const input = await readJSON(filename);
+
+let data;
+try {
+  data = await readJSON(output);
+} catch {
+  data = { ads: [] };
+}
+
+const inputAds = input.docs;
+const existingAds = data.ads;
+
+const adsByID = (i: { ad_id: number }) => i.ad_id;
+
+const currentAds = existingAds.map(adsByID);
+const foundAds = inputAds.map(adsByID);
+
+const difference = R.difference(foundAds, currentAds);
+
+if (difference.length) {
+  const newAds = inputAds.filter((i: { ad_id: number }) =>
+    difference.includes(i.ad_id)
+  );
+  const newData = { ads: [] };
+  newData.ads.push(newAds);
+  await writeJSON(output, newData);
+  // do webhook
+}
 
 removeFile(filename);
