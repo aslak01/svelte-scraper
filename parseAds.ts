f4ad81cdfc2624c907247f628c74c27e3b70aad2
aslak <aslakrb@gmail.com>
2023-01-19 18:34:54 +0100

    fix mapping on undefined?

--- parseAds.ts
+++ parseAds.ts
@@ -2,20 +2,17 @@ import {
   readJSON,
   removeFile,
   writeJSON,
-} from "https://deno.land/x/flat@0.0.14/mod.ts";
+} from "https://deno.land/x/flat@0.0.15/mod.ts";
 
 import * as R from "https://deno.land/x/ramda@v0.27.2/mod.ts";
 
 // The filename is the first invocation argument
-const filename = Deno.args[0];
-const output = `filtered_${filename}`;
+const inputFile = Deno.args[0];
+const outputFile = `filtered_${inputFile}`;
 
-const input = await readJSON(filename);
-
-let data;
-try {
-  data = await readJSON(output);
-} catch {
+const input = await readJSON(inputFile);
+let data = await readJSON(outputFile);
+if (typeof data === "undefined") {
   data = { ads: [] };
 }
 
@@ -35,8 +32,8 @@ if (difference.length) {
   );
   const newData = { ads: [] };
   newData.ads.push(newAds);
-  await writeJSON(output, newData);
+  await writeJSON(outputFile, newData);
   // do webhook
 }
 
-removeFile(filename);
+removeFile(inputFile);
