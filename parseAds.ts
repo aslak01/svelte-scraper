ca5ea8b02aff58fcb9a721dc94d4f6a864cf2734
aslak <aslakrb@gmail.com>
2023-01-19 20:59:49 +0100

    fix?

--- parseAds.ts
+++ parseAds.ts
@@ -8,21 +8,18 @@ import * as R from "https://deno.land/x/ramda@v0.27.2/mod.ts";
 
 // The filename is the first invocation argument
 const inputFile = Deno.args[0];
+
 const outputFile = `filtered_${inputFile}`;
+const inputData = await readJSON(inputFile);
+const existingData = await readJSON(outputFile);
 
-const input = await readJSON(inputFile);
-let data = await readJSON(outputFile);
-if (typeof data === "undefined") {
-  data = { ads: [] };
-}
-
-const inputAds = input.docs;
-const existingAds = data.ads;
+const inputAds = inputData.docs;
+const existingAds = existingData.ads;
 
 const adsByID = (i: { ad_id: number }) => i.ad_id;
 
-const currentAds = existingAds.map(adsByID);
 const foundAds = inputAds.map(adsByID);
+const currentAds = existingAds.map(adsByID);
 
 const difference = R.difference(foundAds, currentAds);
 
@@ -30,10 +27,10 @@ if (difference.length) {
   const newAds = inputAds.filter((i: { ad_id: number }) =>
     difference.includes(i.ad_id)
   );
-  const newData = { ads: [] };
+  const newData = existingData;
   newData.ads.push(newAds);
   await writeJSON(outputFile, newData);
   // do webhook
 }
 
-removeFile(inputFile);
+// removeFile(inputFile);
