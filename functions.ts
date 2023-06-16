7a7353966a90be182255dc9e346a4ccd745e8368
aslak <aslakrb@gmail.com>
2023-06-16 02:39:57 +0200

    refactor code

--- functions.ts
+++ functions.ts
@@ -1,7 +1,12 @@
 import type { FinnAd } from "./types.ts";
 
+const formatToMsg = (ad: FinnAd) => {
+  return "**" + ad.company_name + "** i " + ad.location + ": *" + ad.job_title +
+    "* \n" + "https://www.finn.no/job/fulltime/ad.html?finnkode=" + ad.id;
+};
+
 export const postToWebhook = async (ad: FinnAd, webhookUrl: string) => {
-  const content = { content: adToMsg(ad) };
+  const content = { content: formatToMsg(ad) };
   const resp = await fetch(webhookUrl, {
     method: "POST",
     headers: {
@@ -10,11 +15,5 @@ export const postToWebhook = async (ad: FinnAd, webhookUrl: string) => {
     body: JSON.stringify(content),
   });
 
-  console.log(resp);
-  return resp;
-};
-
-const adToMsg = (ad: FinnAd) => {
-  return "**" + ad.company_name + "** i " + ad.location + ": *" + ad.job_title +
-    "* \n" + "https://www.finn.no/job/fulltime/ad.html?finnkode=" + ad.id;
+  console.log("posted", resp);
 };
--- parseAds.ts
+++ parseAds.ts
@@ -2,56 +2,31 @@ import type { FinnAd } from "./types.ts";
 
 import { postToWebhook } from "./functions.ts";
 
-// import { config as loadEnv } from "https://deno.land/std/dotenv/mod.ts";
-
-import {
-  readJSON,
-  writeJSON,
-  // removeFile,
-} from "https://deno.land/x/flat@0.0.15/mod.ts";
-
+import { readJSON, writeJSON } from "https://deno.land/x/flat@0.0.15/mod.ts";
 import * as R from "https://x.nest.land/ramda@0.27.2/mod.ts";
 
-// const configData = await loadEnv({
-//   export: true,
-//   allowEmptyValues: true,
-// });
-
-// let WEEBHOOK = "";
-// let inputFile;
-
-// if (Deno.args[1]) {
-// running on github with webhook passed as param from actions
 const WEEBHOOK = Deno.args[0];
 const inputFile = Deno.args[1];
-// } else {
-//   WEEBHOOK = configData.WEBHOOK_URL;
-//   inputFile = Deno.args[0];
-// }
 
 const outputFile = `filtered_${inputFile}`;
 const inputData = await readJSON(inputFile);
-const existingData = await readJSON(outputFile);
+const currData = await readJSON(outputFile);
 
 const inputAds = inputData.docs;
-const existingAds = existingData.ads;
+const existingAds = currData.ads;
 
-const adsByID = (i: FinnAd) => i.ad_id;
+const adsByID = <T extends { ad_id: string }>(i: T): string => i.ad_id;
 
 const foundAds = inputAds.map(adsByID);
 const currentAds = existingAds.map(adsByID);
 
 const difference = R.difference(foundAds, currentAds);
 
-if (difference.length) {
-  const newAds = inputAds.filter((i: FinnAd) => difference.includes(i.ad_id));
-  const newData = existingData;
-  newAds.forEach((ad: FinnAd) => {
-    newData.ads.push(ad);
-    postToWebhook(ad, WEEBHOOK);
-  });
+const newAds = inputAds.filter((i: FinnAd) => difference.includes(i.ad_id));
 
-  await writeJSON(outputFile, newData);
-}
+newAds.forEach((ad: FinnAd) => {
+  currData.ads.push(ad);
+  postToWebhook(ad, WEEBHOOK);
+});
 
-// removeFile(inputFile);
+await writeJSON(outputFile, currData);
