42a8b16afea1d092461ed89ea846438b3677aa5c
as <aslak.bakkeland@medialaben.no>
2023-08-26 12:25:11 +0200

    restructure param

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
 import { join } from "./lib/imports.ts";
 import * as io from "./lib/io/index.ts";
 
+// Init routine called with one argument
+// Main routine called with two arguments
 async function main() {
   const start = performance.now();
 
-  if (Deno.args.length < 2) {
+  if (Deno.args.length < 1) {
     console.error(
-      "Please provide the input file and webhook url as parameters",
+      "Please provide an input file, and optionally a webhook url.",
     );
     return;
   }
 
-  if (Deno.args.length > 3) {
+  if (Deno.args.length > 2) {
     console.error(
-      "Too many input parameters provided. Please provide only 2 or 3.",
+      "Too many input parameters provided. Please provide only 1 or 2",
     );
     return;
   }
@@ -35,21 +37,20 @@ async function main() {
     return;
   }
 
-  const webhookPath = Deno.args[1];
-
-  if (!webhookPath || typeof webhookPath !== "string") {
-    console.error("The provided webhook path could not be parsed");
-    return;
-  }
-
   const outputFile = "./out/data.csv";
 
   let inputLines = 0;
 
-  if (Deno.args.length === 3) {
+  if (Deno.args.length === 1) {
     console.info("INITIALISING THE CSV");
     inputLines = await io.initCSVfromJson(inputFile, outputFile);
   } else {
+    const webhookPath = Deno.args[1] || false;
+
+    if (!webhookPath || typeof webhookPath !== "string") {
+      console.error("The provided webhook path could not be parsed");
+      return;
+    }
     console.log("Performing routine");
     const prevFetchFilePath = "./data/prev/";
     const prevFetchFile = join(prevFetchFilePath, "data.json");
