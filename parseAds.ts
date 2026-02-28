2d3518351160e8e5fe53fb2727b9b8ea506bf328
Aslak <hei@asla.cc>
2026-02-28 22:15:09 +0100

    cleanup deps

--- parseAds.ts
+++ parseAds.ts
@@ -2,14 +2,12 @@ import type { FinnAd } from "./types.ts";
 
 import { postToWebhook } from "./functions.ts";
 
-import { readJSON, writeJSON } from "https://deno.land/x/flat@0.0.15/mod.ts";
-
 const WEEBHOOK = Deno.args[0];
 const inputFile = Deno.args[1];
 
 const outputFile = `filtered_${inputFile}`;
-const inputData = await readJSON(inputFile);
-const currData = await readJSON(outputFile);
+const inputData = JSON.parse(await Deno.readTextFile(inputFile));
+const currData = JSON.parse(await Deno.readTextFile(outputFile));
 
 const inputAds = inputData.docs;
 const existingIds = new Set(currData.ads.map((i: FinnAd) => i.ad_id));
@@ -21,4 +19,4 @@ newAds.forEach((ad: FinnAd) => {
   postToWebhook(ad, WEEBHOOK);
 });
 
-await writeJSON(outputFile, currData);
+await Deno.writeTextFile(outputFile, JSON.stringify(currData));
