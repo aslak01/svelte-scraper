3a03f7e27bd4470c00d8342efaf77aa6e0cd9ccf
Aslak <hei@asla.cc>
2026-04-08 18:53:17 +0200

    stop committing so much

--- parseAds.ts
+++ parseAds.ts
@@ -14,9 +14,14 @@ const existingIds = new Set(currData.ads.map((i: FinnAd) => i.ad_id));
 
 const newAds = inputAds.filter((i: FinnAd) => !existingIds.has(i.ad_id));
 
-for (const ad of newAds) {
-  currData.ads.push(ad);
-  await postToWebhook(ad, WEEBHOOK);
+if (newAds.length === 0) {
+  // Restore raw file to committed version so Flat doesn't create an empty commit
+  const git = new Deno.Command("git", { args: ["checkout", "--", inputFile] });
+  await git.output();
+} else {
+  for (const ad of newAds) {
+    currData.ads.push(ad);
+    await postToWebhook(ad, WEEBHOOK);
+  }
+  await Deno.writeTextFile(outputFile, JSON.stringify(currData, null, 2));
 }
-
-await Deno.writeTextFile(outputFile, JSON.stringify(currData, null, 2));
