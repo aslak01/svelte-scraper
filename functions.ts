d21762901ad867e7d216de3eccf00a4acdeddaf1
Aslak <hei@asla.cc>
2026-04-08 18:51:31 +0200

    api route

--- functions.ts
+++ functions.ts
@@ -2,7 +2,7 @@ import type { FinnAd } from "./types.ts";
 
 const formatToMsg = (ad: FinnAd) => {
   return "**" + ad.company_name + "** i " + ad.location + ": *" + ad.job_title +
-    "* \n" + "https://www.finn.no/job/fulltime/ad.html?finnkode=" + ad.id;
+    "* \n" + "https://www.finn.no/job/ad/" + ad.id;
 };
 
 export const postToWebhook = async (ad: FinnAd, webhookUrl: string) => {
